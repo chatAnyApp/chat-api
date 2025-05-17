ea8dc07ce73e670f7f63a0b72508bb641f09d8b5
Ahmet Kadir Aksu <72475558+AhmetKadir@users.noreply.github.com>
2025-05-18 02:24:24 +0300

    Update UserController.java

--- src/main/java/com/chatAny/chatapi/api/UserController.java
+++ src/main/java/com/chatAny/chatapi/api/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:5173", "http://localhost:3000"}, maxAge = 3600)
 @RestController
 @RequestMapping("/users")
 public class UserController {
