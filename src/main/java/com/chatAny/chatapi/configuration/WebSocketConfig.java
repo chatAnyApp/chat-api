c69c825dae195fb094d3c60670eee24d65b0ab47
AhmetKadir <ahmetkadir518@gmail.com>
2025-06-15 16:19:52 +0300

    refactor: update MessageAvro to remove autogenerated comments and change NotNull annotation to Jakarta validation

--- src/main/java/com/chatAny/chatapi/configuration/WebSocketConfig.java
+++ src/main/java/com/chatAny/chatapi/configuration/WebSocketConfig.java
@@ -1,6 +1,6 @@
 package com.chatAny.chatapi.configuration;
 
-import org.jetbrains.annotations.NotNull;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
